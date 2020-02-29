510a3e50591bfbff44fc71f680ab9e50f235c5bb
Jen Varela Lijó <jen.lijo@zalando.de>
2020-02-29 11:47:38 +0100

    Implement User

---
+++ src/main/java/com/pingpong/app/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.pingpong.app.repositories;
+
+import com.pingpong.app.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
